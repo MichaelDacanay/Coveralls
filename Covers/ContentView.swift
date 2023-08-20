2ca6938c654d65dc24fe96a8ed6b9d502b88e131
michaeldacanay <michaeldacanayusa@gmail.com>
2023-08-20 02:55:01 -0400

    MVP product, calculate standard deviation and covered call strike prices

--- Covers/ContentView.swift
+++ Covers/ContentView.swift
@@ -9,24 +9,27 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @State private var stockSymbol = ""
-    @State private var currentPrice = ""
-    @State private var impliedVolatility = ""
+    @State private var stockSymbolString = ""
+    @State private var currentPriceString = ""
+    @State private var impliedVolatilityString = ""
+    @State private var daysUntilExpirationString = ""
     @State private var riskTolerance = 0
     let options = ["Conservative", "Moderate", "Aggressive"]
     
-    
-    @State private var conservativeStrikePrice = ""
-    @State private var moderateStrikePrice = ""
-    @State private var aggressiveStrikePrice = ""
+    @State private var standardDeviation = 0.0
+    @State private var conservativeStrikePrice = 0.0
+    @State private var moderateStrikePrice = 0.0
+    @State private var aggressiveStrikePrice = 0.0
+    @State var computed = false
     
     var body: some View {
         VStack {
             Image("512x512px_covers_title").resizable().frame(width: 200, height: 200)
             
-            TextField("Stock Symbol", text: $stockSymbol).padding([.leading, .bottom])
-            TextField("Current Price", text: $currentPrice).padding([.leading, .bottom])
-            TextField("Implied Volatility", text: $impliedVolatility).padding([.leading, .bottom])
+            TextField("Stock Symbol", text: $stockSymbolString).padding([.leading, .bottom])
+            TextField("Current Price", text: $currentPriceString).padding([.leading, .bottom])
+            TextField("Implied Volatility (between 0 and 1)", text: $impliedVolatilityString).padding([.leading, .bottom])
+            TextField("Days until Expiration", text: $daysUntilExpirationString).padding([.leading, .bottom])
             Picker(selection: $riskTolerance, label: Text("Approach")) {
                 ForEach(0..<options.count, id: \.self) {
                     Text(self.options[$0])
@@ -36,6 +39,21 @@ struct ContentView: View {
             
             Button("Compute") {
                 // my code
+                if let currentPrice = Double(currentPriceString),
+                   let impliedVolatility = Double(impliedVolatilityString),
+                   let daysUntilExpiration = Double(daysUntilExpirationString) {
+                    standardDeviation = currentPrice * impliedVolatility * sqrt(daysUntilExpiration / 365)
+                    aggressiveStrikePrice = ceil(currentPrice)
+                    moderateStrikePrice = currentPrice + standardDeviation
+                    conservativeStrikePrice = currentPrice + 1.282 * standardDeviation
+                    print(standardDeviation)
+                    print(aggressiveStrikePrice)
+                    print(moderateStrikePrice)
+                    print(conservativeStrikePrice)
+                } else {
+                    print("Invalid input")
+                }
+                computed = true
             }.padding(10)
                 .background(Color.accentColor)
                 .foregroundColor(.black)
@@ -44,9 +62,34 @@ struct ContentView: View {
             
             Spacer()
             
+            if conservativeStrikePrice > 0 {
+                HStack {
+                    VStack {
+                        Text("Aggressive")
+                        Text(String(format: "%.2f", aggressiveStrikePrice))
+                    }
+                    VStack {
+                        Text("Moderate")
+                        Text(String(format: "%.2f", moderateStrikePrice))
+                    }
+                    VStack {
+                        Text("Conservative")
+                        Text(String(format: "%.2f", conservativeStrikePrice))
+                    }
+                }.padding()
+            }
+            
+            if computed && (currentPriceString.isEmpty || impliedVolatilityString.isEmpty || daysUntilExpirationString.isEmpty) {
+                Text("Invalid input").foregroundColor(.red)
+            }
+            
+            
+            
         }
     }
 }
+
+
 
 
 
