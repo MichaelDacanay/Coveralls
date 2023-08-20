3db38778c18275d89e5e9a1b6b24f6361758a378
michaeldacanay <michaeldacanayusa@gmail.com>
2023-08-20 01:27:22 -0400

    Initial commit, covers calculator UI

--- Covers/ContentView.swift
+++ Covers/ContentView.swift
@@ -9,79 +9,46 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var stockSymbol = ""
+    @State private var currentPrice = ""
+    @State private var impliedVolatility = ""
+    @State private var riskTolerance = 0
+    let options = ["Conservative", "Moderate", "Aggressive"]
+    
+    
+    @State private var conservativeStrikePrice = ""
+    @State private var moderateStrikePrice = ""
+    @State private var aggressiveStrikePrice = ""
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+        VStack {
+            Image("512x512px_covers_title").resizable().frame(width: 200, height: 200)
+            
+            TextField("Stock Symbol", text: $stockSymbol).padding([.leading, .bottom])
+            TextField("Current Price", text: $currentPrice).padding([.leading, .bottom])
+            TextField("Implied Volatility", text: $impliedVolatility).padding([.leading, .bottom])
+            Picker(selection: $riskTolerance, label: Text("Approach")) {
+                ForEach(0..<options.count, id: \.self) {
+                    Text(self.options[$0])
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            }.pickerStyle(.segmented)
+                .padding()
+            
+            Button("Compute") {
+                // my code
+            }.padding(10)
+                .background(Color.accentColor)
+                .foregroundColor(.black)
+                .bold()
+                .cornerRadius(10)
+            
+            Spacer()
+            
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
