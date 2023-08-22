14312761e5e707a635046b9295e1fad18bcd20eb
michaeldacanay <michaeldacanayusa@gmail.com>
2023-08-22 10:56:53 -0400

    Dismiss keyboard when submitting or tapping on the screen, add descriptive captions to risk tolerance approach

--- Covers/ContentView.swift
+++ Covers/ContentView.swift
@@ -10,12 +10,18 @@ import Combine
 import CoreData
 
 struct ContentView: View {
+    private enum Field: Int, CaseIterable {
+        case stockticker, price, impvolatility, daystoexp
+    }
+    
+    @FocusState private var focusedField: Field?
+    
     @State private var stockSymbolString = ""
     @State private var currentPriceString = ""
     @State private var impliedVolatility: Double = 0.5
-//    @State private var daysUntilExpirationString = ""
     @State private var daysUntilExpiration: Int = 14
     @State private var expirationDate = Date()
+    
     @State private var riskTolerance = 0
     let options = ["Aggressive", "Moderate", "Conservative"]
     
@@ -27,61 +33,63 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Image("512x512px_covers_title").resizable().frame(width: 200, height: 200)
+            Image("350x350px_covers_title_transparent").resizable().frame(width: 200, height: 200)
             
-            TextField("Stock Symbol", text: $stockSymbolString).padding([.leading, .bottom])
-            HStack {
-                Text("Current price:")
-                    .font(.callout)
-                    .bold()
-                TextField("In USD", text: $currentPriceString)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }.padding([.leading, .bottom])
-            
-            HStack {
-                Text("Implied volatility:")
-                    .font(.callout)
-                    .bold()
-                TextField("between 0 and 1",
-                          value: $impliedVolatility,
-                          formatter: NumberFormatter.decimalFormatter)
-                    .keyboardType(.decimalPad)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }.padding([.leading, .bottom])
-            
-            
-            // date
-            Group {
-                HStack {
-                    Text("Days until expiration:")
-                        .font(.callout)
-                        .bold()
-                    TextField("Or select expiration date",
-                              value: $daysUntilExpiration,
-                              formatter: NumberFormatter())
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                }.padding([.leading, .bottom])
+            Form {
+                TextField("Stock ticker", text: $stockSymbolString)
+                    .focused($focusedField, equals: .stockticker)
                 
-                DatePicker("Exp date",
-                           selection: $expirationDate,
-                           displayedComponents: .date)
-                    .onReceive(Just(expirationDate)) { date in
-                                        print("Selected date: \(date)")
-                                        print("Today's date: \(Date())")
-                                        
-                        // Run your Swift code here
-                        daysUntilExpiration = Int(Double(expirationDate - Date()) / 86400)
-                                        print("difference: \(daysUntilExpiration)")
-                }.padding([.leading, .bottom, .trailing])
-            }
-            
-            // approach
-            Picker(selection: $riskTolerance, label: Text("Approach")) {
-                ForEach(0..<options.count, id: \.self) {
-                    Text(self.options[$0])
+                HStack {
+                    Text("Current price:")
+                        .font(.callout)
+                    TextField("USD", text: $currentPriceString)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .focused($focusedField, equals: .price)
                 }
-            }.pickerStyle(.segmented)
-                .padding()
+                
+                HStack {
+                    Text("Implied volatility (0-1):")
+                        .font(.callout)
+                    TextField("% divide by 100", value: $impliedVolatility,
+                              formatter: NumberFormatter.decimalFormatter)
+                        .keyboardType(.decimalPad)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .focused($focusedField, equals: .impvolatility)
+                }
+                
+                // date
+                Group {
+                    HStack {
+                        Text("Days until expiration:")
+                            .font(.callout)
+                        TextField("Or select expiration date",
+                                  value: $daysUntilExpiration,
+                                  formatter: NumberFormatter())
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .focused($focusedField, equals: .daystoexp)
+                    }
+                    
+                    DatePicker("Exp. date",
+                               selection: $expirationDate,
+                               displayedComponents: .date)
+                        .onChange(of: expirationDate) {
+                            focusedField = nil
+                            print("Selected date: \($0)")
+                            print("Today's date: \(Date())")
+                            
+                            // Run your Swift code here
+                            daysUntilExpiration = Int(Double(expirationDate - Date()) / 86400)
+                            print("difference: \(daysUntilExpiration)")
+                        }
+                }
+                
+                // approach
+                Picker(selection: $riskTolerance, label: Text("Approach")) {
+                    ForEach(0..<options.count, id: \.self) {
+                        Text(self.options[$0])
+                    }
+                }.pickerStyle(.segmented)
+            }
             
             Button("Compute") {
                 // my code
@@ -98,6 +106,7 @@ struct ContentView: View {
                     print("Invalid input")
                 }
                 computed = true
+                focusedField = nil
             }.padding(10)
                 .background(Color.accentColor)
                 .foregroundColor(.black)
@@ -110,14 +119,20 @@ struct ContentView: View {
                 HStack {
                     VStack {
                         Text("Aggressive")
+                        Text("Highest premium, 50/50 chance of keeping or losing stocks")
+                            .font(.caption)
                         Text(String(format: "%.2f", aggressiveStrikePrice))
                     }
                     VStack {
                         Text("Moderate")
+                        Text("1 standard deviation higher, 16% to lose stocks")
+                            .font(.caption)
                         Text(String(format: "%.2f", moderateStrikePrice))
                     }
                     VStack {
                         Text("Conservative")
+                        Text("10% chance to lose stocks")
+                            .font(.caption)
                         Text(String(format: "%.2f", conservativeStrikePrice))
                     }
                 }.padding()
@@ -127,6 +142,9 @@ struct ContentView: View {
                 Text("Invalid input").foregroundColor(.red)
             }
             
+        }
+        .onTapGesture {
+            hideKeyboard()
         }
     }
 }
@@ -146,6 +164,13 @@ extension NumberFormatter {
 extension Date {
     static func - (lhs: Date, rhs: Date) -> TimeInterval {
         return lhs.timeIntervalSinceReferenceDate - rhs.timeIntervalSinceReferenceDate
+    }
+}
+
+extension View {
+    func hideKeyboard() {
+        let resign = #selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
     }
 }
 
